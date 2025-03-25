56ece7409ba2210c5e986107347bb32c6b592c0e
TurtleBr4 <106892033+TurtleBr4@users.noreply.github.com>
2025-03-25 09:47:26 -0400

    morning work

--- AppExten3/Assets/Scripts/Player/Player.cs
+++ AppExten3/Assets/Scripts/Player/Player.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public Camera mainCamera;
+    public Transform playerHead;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +13,23 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        RotateToMouse();
+    }
+
+    void RotateToMouse()
+    {
+        // Get mouse position in world space
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        Plane plane = new Plane(Vector3.up, playerHead.position); // Plane at object's height
+
+        if (plane.Raycast(ray, out float distance))
+        {
+            Vector3 targetPoint = ray.GetPoint(distance);
+            Vector3 direction = (targetPoint - playerHead.position).normalized;
+
+            // Rotate towards the mouse
+            Quaternion lookRotation = Quaternion.LookRotation(-direction, Vector3.up);
+            playerHead.rotation = Quaternion.Slerp(playerHead.rotation, lookRotation, Time.deltaTime * 10f);
+        }
     }
 }
