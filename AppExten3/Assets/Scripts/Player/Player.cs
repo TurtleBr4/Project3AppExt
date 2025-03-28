a2dac4470cc310c199008755f8fe761cfb20fa92
TurtleBr4 <106892033+TurtleBr4@users.noreply.github.com>
2025-03-28 04:04:25 -0400

    Animations oh my god

--- AppExten3/Assets/Scripts/Player/Player.cs
+++ AppExten3/Assets/Scripts/Player/Player.cs
@@ -2,23 +2,45 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    //Setup
     public Camera mainCamera;
     public Transform playerHead;
+    private CharacterController controller;
+    //public Transform playerModel;
+
+    //Movement
+    public float moveSpeed = 10f;
+    public float runSpeed;
+    public float gravity = -9.81f;
+    public bool isRunning = false;
+    private Vector3 velocity;
+    private Vector3 movementInput;
+
+
+    //Animation and Attacking
+    private Animator anim;
+    public bool isMoving = false;
+    public bool isAttacking = false;
+    
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
+        runSpeed = 2 * moveSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
         RotateToMouse();
+        goMove();
     }
 
     void RotateToMouse()
     {
-        // Get mouse position in world space
+        // Find the mouse position in the world
         Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
         Plane plane = new Plane(Vector3.up, playerHead.position); // Plane at object's height
 
@@ -32,4 +54,56 @@ public class Player : MonoBehaviour
             playerHead.rotation = Quaternion.Slerp(playerHead.rotation, lookRotation, Time.deltaTime * 10f);
         }
     }
+
+    void goMove()
+    {
+        anim.SetInteger("Direction", 5);
+        isRunning = Input.GetKey(KeyCode.LeftShift);
+        float speed = isRunning ? runSpeed : moveSpeed;
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        if (horizontal == 0 && vertical == 0)
+        {
+            movementInput = Vector3.zero;
+            anim.SetBool("Moving", false);
+        }
+        else
+        {
+            anim.SetBool("Moving", true);
+            movementInput = (transform.right * horizontal + transform.forward * vertical).normalized * speed;
+        }
+
+        if(horizontal > 0)
+        {
+            anim.SetInteger("Direction", 2);
+        }
+        else if (horizontal < 0)
+        {
+            anim.SetInteger("Direction", 1);
+        }
+        if (vertical > 0)
+        {
+            anim.SetInteger("Direction", 0);
+        }
+        else if (vertical < 0)
+        {
+            anim.SetInteger("Direction", 3);
+        } //not my proudest code block, no sir
+
+        velocity.y += gravity * Time.deltaTime;
+        Vector3 move = movementInput * Time.deltaTime;
+        move.y = velocity.y * Time.deltaTime;
+        controller.Move(move);
+        
+        
+
+    }
+
+    void attackNow()
+    {
+
+    }
+
+
 }
